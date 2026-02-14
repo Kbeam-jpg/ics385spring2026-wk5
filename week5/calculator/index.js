84b613c1964ae4c8a7d54edd6345bde7652305d5
Kendall Beam <beamk@hawaii.edu>
2026-02-14 03:33:01 +0000

    wk5c done

--- week5/calculator/index.js
+++ week5/calculator/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/VolCalculator.html");
+});
 
 // on: load page req
 // get -> res send volCalc html file
@@ -21,8 +25,10 @@ app.post("/VolCalc", function (req, res) {
   let vol = pi * Math.pow(r, 2) * h;
 
   // display as field on webpage
+  //format result to 2 decimal places
+  res.send(`Volume = ${vol.toFixed(2)}`);
 });
 
 app.listen(PORT, function () {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
